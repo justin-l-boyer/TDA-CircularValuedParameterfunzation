915bcd964f2bf4697e01d199822e2feeab1a40e7
wilson <justinlboyer@gmail.com>
2017-03-25 23:52:04 -0600

    Changed the loop to count one more, forced output to be float

--- timeDelayEmbedding.py
+++ timeDelayEmbedding.py
@@ -9,11 +9,11 @@ import numpy as np
 from scipy import sparse
 def embed(time_series, tau_max, embedding_length, delay=1):
     out=[]
-    for tau in range(delay, tau_max):
+    for tau in range(delay, tau_max+1):
         #unlagged = time_series[:-tau]
         lagged = np.roll(time_series, -tau, axis=0)[:-tau]
         lagged = lagged[0:embedding_length]
         out.append(lagged.tolist())    
-    out = np.array(out).transpose()
+    out = np.array(out, dtype=float).transpose()
     return(out)
         
