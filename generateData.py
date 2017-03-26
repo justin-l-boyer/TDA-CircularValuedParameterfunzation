fc235549771a5c25e3cde48e7e5325da4ddfe124
wilson <justinlboyer@gmail.com>
2017-03-25 23:49:27 -0600

    created a sine wave that has incommenserate wavelength

--- generateData.py
+++ generateData.py
@@ -8,9 +8,9 @@ Created on Thu Mar  9 16:52:35 2017
 import numpy as np
 import pandas as pd
 # this script generates the sin data
-def cosineData(start, stop, number_points, Noise=True, mean=0, std_dev=0.25):
+def sineData(start, stop, number_points, wavelength, Noise=True, mean=0, std_dev=0.25):
     t = np.linspace(start, stop, number_points)
-    y = np.cos(2*np.pi*t)
+    y = np.sin(2*np.pi/float(wavelength)*t)
     if Noise==True:
         noise = np.random.normal(mean, std_dev, len(t))
         y = y + noise
@@ -18,3 +18,13 @@ def cosineData(start, stop, number_points, Noise=True, mean=0, std_dev=0.25):
     V = V.as_matrix()
     return(V)
     
+def incomSineData(start, stop, number_points, wavelength, Noise=False, mean=0, std_dev=0.25):
+    t = np.linspace(start, stop, number_points)
+    y=np.zeros(len(t))
+    y = np.sin(2*np.pi/float(wavelength)*t)+np.sin(20*t/float(wavelength))
+    if Noise==True:
+        noise = np.random.normal(mean, std_dev, len(t))
+        y = y + noise
+    V = pd.DataFrame([t,y]).transpose()
+    V = V.as_matrix()
+    return(V)
