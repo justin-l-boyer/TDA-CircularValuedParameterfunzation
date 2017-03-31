94a98aaa1b9a83a1e3e44b675c71a95b5f34f1a3
Justin Boyer <justinlboyer@gmail.com>
2017-03-31 04:57:32 -0600

    removed spurious entries

--- timeDelayEmbedding.py
+++ timeDelayEmbedding.py
@@ -9,19 +9,12 @@ import numpy as np
 from scipy import sparse
 def embed(time_series, tau_max, embedding_length, delay=1):
     out=[]
-<<<<<<< HEAD
     for tau in range(delay, tau_max+1):
-=======
-    for tau in range(delay, tau_max):
->>>>>>> fed1a1fde26c5cce57ebc7e20921986502c5eddb
+#    for tau in range(delay, tau_max):
         #unlagged = time_series[:-tau]
         lagged = np.roll(time_series, -tau, axis=0)[:-tau]
         lagged = lagged[0:embedding_length]
         out.append(lagged.tolist())    
-<<<<<<< HEAD
     out = np.array(out, dtype=float).transpose()
-=======
-    out = np.array(out).transpose()
->>>>>>> fed1a1fde26c5cce57ebc7e20921986502c5eddb
+    #out = np.array(out).transpose()
     return(out)
-        
