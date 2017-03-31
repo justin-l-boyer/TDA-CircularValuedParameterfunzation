ee7c82e771e3fe30deb557415a7d8518ce5159cc
Justin Boyer <justinlboyer@gmail.com>
2017-03-31 04:57:23 -0600

    removed spurious entries

--- generateData.py
+++ generateData.py
@@ -8,15 +8,18 @@ Created on Thu Mar  9 16:52:35 2017
 import numpy as np
 import pandas as pd
 # this script generates the sin data
-<<<<<<< HEAD
 def sineData(start, stop, number_points, wavelength, Noise=True, mean=0, std_dev=0.25):
     t = np.linspace(start, stop, number_points)
     y = np.sin(2*np.pi/float(wavelength)*t)
-=======
+    if Noise==True:
+        noise = np.random.normal(mean, std_dev, len(t))
+        y = y + noise
+    V = pd.DataFrame([t,y]).transpose()
+    V = V.as_matrix()
+    return(V)
 def cosineData(start, stop, number_points, Noise=True, mean=0, std_dev=0.25):
     t = np.linspace(start, stop, number_points)
     y = np.cos(2*np.pi*t)
->>>>>>> fed1a1fde26c5cce57ebc7e20921986502c5eddb
     if Noise==True:
         noise = np.random.normal(mean, std_dev, len(t))
         y = y + noise
@@ -24,7 +27,6 @@ def cosineData(start, stop, number_points, Noise=True, mean=0, std_dev=0.25):
     V = V.as_matrix()
     return(V)
     
-<<<<<<< HEAD
 def incomSineData(start, stop, number_points, wavelength, Noise=False, mean=0, std_dev=0.25):
     t = np.linspace(start, stop, number_points)
     y=np.zeros(len(t))
@@ -35,5 +37,3 @@ def incomSineData(start, stop, number_points, wavelength, Noise=False, mean=0, s
     V = pd.DataFrame([t,y]).transpose()
     V = V.as_matrix()
     return(V)
-=======
->>>>>>> fed1a1fde26c5cce57ebc7e20921986502c5eddb
